b2d5a70f67ec6e0e669286ff306fdf83c1c71905
jeff <jeff.etale@student.moringaschool.com>
2023-07-23 19:54:24 +0300

    moved database to public folder, data is now displaying on the UI

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import TransactionForm from "./TransactionForm";
 import TransactionSearch from "./TransactionSearch";
 import TransactionTable from "./TransactionTable";
 
 const App = () => {
   const [transactions, setTransactions] = useState([]);
-  const [searchInput, setSearchInput] = useState('')
+  const [searchInput, setSearchInput] = useState('');
 
   useEffect(() => {
-    fetch('../db.json')
-         .then((r) => r.json())
-         .then((data) => setTransactions(transactions))
-         .catch((error) => console.error('Error fetching data:', error));
-  },[]);
+    fetch('/db.json')
+      .then((response) => response.json())
+      .then((data) => setTransactions(data.transactions))
+      .catch((error) => console.error('Error fetching data:', error));
+  }, []);
 
   const addTransaction = (newTransaction) => {
     newTransaction.id = transactions.length + 1;
@@ -20,15 +20,15 @@ const App = () => {
   };
 
   const transactionsFilter = transactions.filter((transaction) =>
-     transaction.description.includes(searchInput)
+    transaction.description.includes(searchInput)
   );
-  
+
   return (
     <>
       <h1>FLAT IRON BANK</h1>
-      <TransactionForm addTransaction={addTransaction}/>
+      <TransactionForm addTransaction={addTransaction} />
       <TransactionSearch searchInput={searchInput} setSearchInput={setSearchInput} />
-      <TransactionTable transactions={transactionsFilter}/>
+      <TransactionTable transactions={transactionsFilter} />
     </>
   );
 };
