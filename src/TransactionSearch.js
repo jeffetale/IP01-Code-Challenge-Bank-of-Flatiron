450e8386b14c43ff6718a488335fe93288c46477
jeff <jeff.etale@student.moringaschool.com>
2023-07-23 10:57:59 +0300

    A component for searching transactions has been added

---
+++ src/TransactionSearch.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const TransactionSearch = ({searchInput, setSearchInput}) => {
+    const handleChange = (e) => {
+        setSearchInput(e.target.value)
+    };
+
+    return (
+        <input
+        type='text'
+        placeholder='Search Transaction'
+        value={searchInput}
+        onChange={handleChange}
+        />
+    );
+};
+
+export default TransactionSearch;
